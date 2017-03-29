ba826dc43e4d5ee689654f6bfd15b1989df37e5b
John <bronty.95@gmail.com>
2017-03-29 19:11:44 +1100

    test

--- Lab1/Sources/packet.c
+++ Lab1/Sources/packet.c
@@ -92,7 +92,7 @@ bool Packet_Get(void){
 bool Packet_Put(const uint8_t command, const uint8_t parameter1, const uint8_t parameter2, const uint8_t parameter3){
   //calculates checksum from command and parameters and sends all to FIFO
   return (UART_OutChar(command) 
-	& UART_OutChar(parameter1) 
+    & UART_OutChar(parameter1) 
 	& UART_OutChar(parameter2) 
     & UART_OutChar(parameter3) 
     & UART_OutChar(command ^ parameter1 ^ parameter2 ^ parameter3));
